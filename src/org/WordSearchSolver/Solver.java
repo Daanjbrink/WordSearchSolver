b9dc8924e6d38eb6b377c925ebd8a9e6d64726b8
Daanjbrink <daanjbrink@live.nl>
2016-03-01 00:00:04 +0100

    Field can be loaded from a text file

--- src/org/WordSearchSolver/Solver.java
+++ src/org/WordSearchSolver/Solver.java
@@ -1,5 +1,6 @@
 package org.WordSearchSolver;
 
+import java.io.File;
 import java.util.Scanner;
 
 /**
@@ -10,16 +11,62 @@ public class Solver {
     Scanner in = new Scanner(System.in);
 
     public Solver() {
-        int width, height;
 
-        System.out.print("Please enter the width of the game: ");
-        width = Integer.parseInt(in.nextLine());
-        System.out.print("Please enter the height of the game: ");
-        height = Integer.parseInt(in.nextLine());
+        System.out.print("Load from file? ");
 
-        initSolve(width, height);
+        if (in.nextLine().equals("y") || in.nextLine().equals("Y") || in.nextLine().equals("yes")) {
+            try {
+                Scanner reader = new Scanner(new File("C:\\Users\\Rhino\\Desktop\\map.txt"));
 
-        //Ask for words and put them in a map, lets test with "hey"
+                int width = 0, height = 0, wpos = 0, hpos = 0;
+
+                if (reader.hasNextLine()) {
+                    String[] wh = reader.nextLine().split(" ");
+                    width = Integer.parseInt(wh[0]);
+                    height = Integer.parseInt(wh[1]);
+                }
+
+                char[][] field = new char[width][height];
+
+                while (reader.hasNextLine()) {
+                    String[] tmp = reader.nextLine().split(" ");
+
+                    if (tmp[0].equals("Word: ")) {
+                        // Add word, this is on the bottom of the file
+                        continue;
+                    }
+
+                    for (int i = 0; i != tmp.length; i++)
+                        field[wpos + i][hpos] = tmp[i].charAt(0);
+
+                    hpos++;
+                    if (hpos == height)
+                        break;
+                }
+
+                if (width == 0 || height == 0)
+                    return;
+
+                initSolve(field);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            int width, height;
+
+            System.out.print("Please enter the width of the game: ");
+            width = Integer.parseInt(in.nextLine());
+            System.out.print("Please enter the height of the game: ");
+            height = Integer.parseInt(in.nextLine());
+
+            if (width == 0 || height == 0)
+                return;
+
+            //Ask for words and put them in a map
+
+            initSolve(width, height);
+        }
     }
 
     private void initSolve(int width, int height) {
@@ -43,6 +90,20 @@ public class Solver {
         Solve(field);
     }
 
+    private void initSolve(char[][] field) {
+        int width, height;
+        width = field.length;
+        height = field[0].length;
+        for (int i = 0; i != height; i++) {
+            System.out.println();
+            for (int j = 0; j != width; j++) {
+                System.out.print(field[j][i] + " ");
+            }
+        }
+
+        Solve(field);
+    }
+
     private void Solve(char[][] field) {
         int width = field.length;
         int height = field[0].length;
@@ -52,7 +113,7 @@ public class Solver {
                 // Add for loop through word map
                 if (field[j][i] == "h".charAt(0)) {
                     for (Direction dir : Direction.values()) {
-                        Recursive(field, j, i, "ha", dir, 1);
+                        Recursive(field, j, i, "half", dir, 1);
                     }
                 }
             }
